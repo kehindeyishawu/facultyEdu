085898dc15ac339353c8866e5c89b6a2b9bf3789
Kenny <fkennyf@gmail.com>
2019-04-05 21:57:53 +0100

    setup

---
+++ app.js
@@ -0,0 +1,31 @@
+// requiring index
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const express = require("express");
+const app       =   express();
+const bodyParser  = require("body-parser");
+const mongoose    = require("mongoose");
+// ********************************
+
+// requiring routes
+const indexRoute = require("./routes/index")
+
+// ***********************************
+
+const url = process.env.EDU || "mongodb://localhost:27017/faculty_edu";
+// App config
+app.use(express.static(__dirname + "public"));
+app.set("view engine", "ejs");
+mongoose.connect(url, {useNewUrlParser: true});
+app.use(bodyParser.urlencoded({extended: true}));
+// *****************
+
+// using routes
+app.use("/", indexRoute)
+// *********************
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
