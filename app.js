2b184e01744aa372d6307c4be313e30ab8936be0
kenny <fkennyf@gmail.com>
2019-05-02 21:04:17 +0100

    heroku setupee

--- app.js
+++ app.js
@@ -1,73 +1,86 @@
-// requiring index
-// const http = require('http');
-const hostname                    = process.env.IP || '127.0.0.1';
-const port                        =  process.env.PORT || 3000;
+// // requiring index
+// // const http = require('http');
+// const hostname                    = process.env.IP || '127.0.0.1';
+// const port                        =  process.env.PORT || 3000;
 
-const express                     = require("express");
-const app                         =   express();
-const methodOverride              =   require("method-override");
-const bodyParser                  = require("body-parser");
-const mongoose                    = require("mongoose");
-const passport                    = require("passport");
-const LocalStrategy               = require("passport-local");
-const passportLocalMongoose       = require("passport-local-mongoose");
-const flash                       = require("connect-flash");
-// requiring and configing dotenv
-const dotenv                      = require('dotenv')
-const result = dotenv.config()
+// const express                     = require("express");
+// const app                         =   express();
+// const methodOverride              =   require("method-override");
+// const bodyParser                  = require("body-parser");
+// const mongoose                    = require("mongoose");
+// const passport                    = require("passport");
+// const LocalStrategy               = require("passport-local");
+// const passportLocalMongoose       = require("passport-local-mongoose");
+// const flash                       = require("connect-flash");
+// // requiring and configing dotenv
+// const dotenv                      = require('dotenv')
+// const result = dotenv.config()
  
-if (result.error) {
-  throw result.error
-}
+// if (result.error) {
+//   throw result.error
+// }
  
-console.log(result.parsed)
-// ********************************
-// REQUIRING MODELS
-const User = require("./models/user")
-// *************************
-// requiring routes
-const indexRoute = require("./routes/index")
-const achievementRoute = require("./routes/achievement")
+// console.log(result.parsed)
+// // ********************************
+// // REQUIRING MODELS
+// const User = require("./models/user")
+// // *************************
+// // requiring routes
+// const indexRoute = require("./routes/index")
+// const achievementRoute = require("./routes/achievement")
 
-// ***********************************
+// // ***********************************
 
-const url = process.env.EDU || "mongodb://localhost:27017/faculty_edu";
-// App config
-app.use(express.static(__dirname + "/public"));
-app.set("view engine", "ejs");
-mongoose.connect(url, {useNewUrlParser: true});
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-app.use(bodyParser.urlencoded({extended: true}));
-// *****************
+// const url = process.env.EDU || "mongodb://localhost:27017/faculty_edu";
+// // App config
+// app.use(express.static(__dirname + "/public"));
+// app.set("view engine", "ejs");
+// mongoose.connect(url, {useNewUrlParser: true});
+// mongoose.set('useFindAndModify', false);
+// mongoose.set('useCreateIndex', true);
+// app.use(bodyParser.urlencoded({extended: true}));
+// // *****************
 
-// PASSPORT CONFIGURATION
-app.use(require("express-session")({
-  secret: "good",
-  resave: false,
-  saveUninitialized: false
-}))
+// // PASSPORT CONFIGURATION
+// app.use(require("express-session")({
+//   secret: "good",
+//   resave: false,
+//   saveUninitialized: false
+// }))
 
-app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-// ****************************
+// app.use(flash());
+// app.use(passport.initialize());
+// app.use(passport.session());
+// passport.use(new LocalStrategy(User.authenticate()));
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+// // ****************************
 
-app.use(function(req, res, next){
-  res.locals.currentUser = req.user;
-  res.locals.success = req.flash("success");
-  res.locals.error = req.flash("error");
-  next();
-});
+// app.use(function(req, res, next){
+//   res.locals.currentUser = req.user;
+//   res.locals.success = req.flash("success");
+//   res.locals.error = req.flash("error");
+//   next();
+// });
 
-// using routes
-app.use(indexRoute);
-app.use(achievementRoute);
-// *********************
+// // using routes
+// app.use(indexRoute);
+// app.use(achievementRoute);
+// // *********************
 
-app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-  });
+// app.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+//   });
+
+const cool = require('cool-ascii-faces')
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('index/home'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
